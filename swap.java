6dd818ebe1759c7f2af15f1695798d3b1c0dbf75
Nandhini48 <nandhininandhuas@gmail.com>
2016-08-31 11:43:31 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int a=10,b=14;
+     int temp;
+     temp=a;
+     a=b;
+     b=temp;
+        System.out.println(""+a);
+        System.out.println(""+b);        
+    }
